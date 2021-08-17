02bf118efacfb8aadc0008fb8109f49b043e8c4f
O'Connor Imogen <IOConnor@GEMCSU.NHS.UK>
2021-08-17 11:19:55 +0100

    Added content to rmarkdown file.

--- render-SMT-report.R
+++ render-SMT-report.R
@@ -1,4 +1,3 @@
 rmarkdown::render(input = "SMT-Dental-Report-Slides.Rmd",
                   output_format = "beamer_presentation",
                   output_file = paste0("reports/SMT-Dental-Report-",Sys.Date(),".pdf"))
-
