ee94992d2cc48fd4dc4d4461be5cdb70ccafd2c9
ImogenConnorHelleur <IOConnor@GEMCSU.NHS.UK>
2021-09-09 18:26:28 +0100

    Added more functions and tidied.  Still needs a refactor.

--- format_new_data.R
+++ format_new_data.R
@@ -1,32 +1,89 @@
 library(tidyverse)
 
-#remove spaces from column names
-colnames(dental_data_all_months) <- make.names(colnames(dental_data_all_months), unique = T)
-colnames(dental_data_august) <- make.names(colnames(dental_data_august), unique = T)
-colnames(orthodontic_data_august) <- make.names(colnames(orthodontic_data_august), unique = T)
+# #function to get dental data into the right format for slide 4
+# get_into_slide4_format <- function(data = dental_data_combined, remove_prototypes = T){
+#   
+#   #remove spaces from column names
+#   colnames(data) <- make.names(colnames(data), unique = T)
+#   
+#   #remove prototype contracts if specified
+#   if(remove_prototypes){
+#     #create not in function
+#     `%notin%` = Negate(`%in%`)
+#     data <- data %>%
+#       filter(Contract.Number %notin% prototype_contracts$proto_contracts)
+#   }
+# 
+#   #group by month and sum UDAs delivered
+#   UDAs_delivered <- data %>%
+#     group_by(Year.Month) %>%
+#     summarise(monthly_UDA_UOAs_delivered = sum(UDA.Delivered, na.rm = T))
+# 
+#   #group by month and sum contracted UDAs
+#   UDAs_contracted <- data %>%
+#     group_by(Year.Month) %>%
+#     summarise(annual_contracted_UDA_UOA = sum(Annual.contracted.UDA, na.rm = T))
+#   
+#   data <- full_join(UDAs_delivered, UDAs_contracted, by = "Year.Month")
+#   data <- data %>%
+#     mutate(month = if_else(Year.Month == 202104, as.Date("2021-04-01"), 
+#                            if_else(Year.Month == 202105, as.Date("2021-05-01"),
+#                                    if_else(Year.Month == 202106, as.Date("2021-06-01"),
+#                                            if_else(Year.Month == 202107, as.Date("2021-07-01"),
+#                                                    if_else(Year.Month == 202108, as.Date("2021-08-01"),
+#                                                            as.Date(NA))))))
+#     )
+#   
+# 
+#   data <- data %>%
+#     mutate(month = as.Date(month)) %>%
+#     #mutate(target_UDA_UOAs_delivered_by_sept = (annual_contracted_UDA_UOA) * septemberTarget / 100) %>%
+#     mutate(perc_of_UDA_UOA_target_delivered = monthly_UDA_UOAs_delivered * 100 / target_UDA_UOAs_delivered_by_sept) %>%
+#     mutate(perc_of_UDA_UOA_target_delivered = round(perc_of_UDA_UOA_target_delivered, 1))
+# }
 
-#create not in function
-`%notin%` = Negate(`%in%`)
-
-#filter out prototype contracts
-dental_data <- filter(dental_data_august, Contract.Number %notin% prototype_contracts$proto_contracts)
-orth_data <- filter(orthodontic_data_august, Contract.Number %notin% prototype_contracts$proto_contracts)
 
 
-annual_contracted_UDA_august <- sum(dental_data$Annual.contracted.UDA, na.rm = T)
-UDA_delivered_august <- sum(dental_data$AUG.UDA.Delivered, na.rm = T)
+# 
+# 
+# #function to get dental data into the right format for slide 4
+# get_into_slide6_format <- function(data = orthodontic_data_combined, remove_prototypes = T){
+#   
+#   #remove spaces from column names
+#   colnames(data) <- make.names(colnames(data), unique = T)
+#   
+#   #remove prototype contracts if specified
+#   if(remove_prototypes){
+#     #create not in function
+#     `%notin%` = Negate(`%in%`)
+#     data <- data %>%
+#       filter(Contract.Number %notin% prototype_contracts$proto_contracts)
+#   }
+#   
+#   #group by month and sum UDAs delivered
+#   UOAs_delivered <- data %>%
+#     group_by(Year.Month) %>%
+#     summarise(monthly_UDA_UOAs_delivered = sum(UOA, na.rm = T))
+#   
+#   #group by month and sum contracted UDAs
+#   UOAs_contracted <- data %>%
+#     group_by(Year.Month) %>%
+#     summarise(annual_contracted_UDA_UOA = sum(Annual.contracted.UOA, na.rm = T))
+#   
+#   slide6_UOA <- full_join(UOAs_delivered, UOAs_contracted, by = "Year.Month")
+#   slide6_UOA <- slide6_UOA %>%
+#     mutate(month = Year.Month)
+# }
 
-annual_contracted_UOA_august <- sum(orth_data$Annual.contracted.UOA, na.rm = T)
-UOA_delivered_august <- sum(orth_data$UOA...AUG..2021., na.rm = T)
 
 
+################################################################################
 #function to get dental data into the right format for slide 4
-get_into_slide4_format <- function(data = dental_data_all_months, remove_prototypes = T){
-  
+get_into_slide4_format_calendar <- function(data = dental_data_combined_calendar, remove_prototypes = T){
+
   #remove spaces from column names
   colnames(data) <- make.names(colnames(data), unique = T)
-  data
-  
+
   #remove prototype contracts if specified
   if(remove_prototypes){
     #create not in function
@@ -37,9 +94,61 @@ get_into_slide4_format <- function(data = dental_data_all_months, remove_prototy
 
   #group by month and sum UDAs delivered
   UDAs_delivered <- data %>%
+    group_by(Month) %>%
+    summarise(monthly_UDA_UOAs_delivered = sum(UDA.Total, na.rm = T)) %>%
+    mutate(target_UDA_UOAs_delivered_by_sept = 25313404) %>%
+    rename(month = Month)
+
+}
+
+
+################################################################################
+#function to get dental data into the right format for slide 4
+get_into_slide6_format_calendar <- function(data = orthodontic_data_combined_calendar, remove_prototypes = F){
+  
+  #remove spaces from column names
+  colnames(data) <- make.names(colnames(data), unique = T)
+  
+  #remove prototype contracts if specified
+  if(remove_prototypes){
+    #create not in function
+    `%notin%` = Negate(`%in%`)
+    data <- data %>%
+      filter(Contract.Number %in% prototype_contracts_orth$proto_contracts)
+  }
+  
+  #group by month and sum UDAs delivered
+  UOAs_delivered <- data %>%
+    group_by(Month) %>%
+    summarise(monthly_UDA_UOAs_delivered = sum(UOA.Total, na.rm = T)) %>%
+    # mutate(target_UDA_UOAs_delivered_by_sept = 828452) %>%
+    mutate(target_UDA_UOAs_delivered_by_sept = 1661898) %>%
+    rename(month = Month)
+  
+}
+
+
+
+################################################################################
+#function to get dental data into the right format for slide 4
+get_into_slide5_format <- function(data = dental_data_combined, remove_prototypes = T){
+  
+  #remove spaces from column names
+  colnames(data) <- make.names(colnames(data), unique = T)
+  
+  #remove prototype contracts if specified
+  if(remove_prototypes){
+    #create not in function
+    `%notin%` = Negate(`%in%`)
+    data <- data %>%
+      filter(Contract.Number %notin% prototype_contracts$proto_contracts)
+  }
+  
+  #group by month and sum UDAs delivered
+  UDAs_delivered <- data %>%
     group_by(Year.Month) %>%
     summarise(monthly_UDA_UOAs_delivered = sum(UDA.Delivered, na.rm = T))
-
+  
   #group by month and sum contracted UDAs
   UDAs_contracted <- data %>%
     group_by(Year.Month) %>%
@@ -54,4 +163,166 @@ get_into_slide4_format <- function(data = dental_data_all_months, remove_prototy
                                                    if_else(Year.Month == 202108, as.Date("2021-08-01"),
                                                            as.Date(NA))))))
     )
+}
+
+
+
+################################################################################
+get_into_slide7_format <- function(data = orthodontic_data_combined, remove_prototypes = F){
+  
+  #remove spaces from column names
+  colnames(data) <- make.names(colnames(data), unique = T)
+  
+  #remove prototype contracts if specified
+  if(remove_prototypes){
+    #create not in function
+    `%notin%` = Negate(`%in%`)
+    data <- data %>%
+      filter(Contract.Number %in% prototype_contracts_orth$proto_contracts)
+  }
+  
+  #group by month and sum UDAs delivered
+  UOAs_delivered <- data %>%
+    group_by(Year.Month) %>%
+    summarise(monthly_UDA_UOAs_delivered = sum(UOA, na.rm = T))
+  
+  #group by month and sum contracted UDAs
+  UOAs_contracted <- data %>%
+    group_by(Year.Month) %>%
+    summarise(annual_contracted_UDA_UOA = sum(Annual.contracted.UOA, na.rm = T))
+  
+  slide7_UOA <- full_join(UOAs_delivered, UOAs_contracted, by = "Year.Month")
+  slide7_UOA <- slide7_UOA %>%
+    rename(month = "Year.Month") %>%
+    mutate(perc_UDA_UOA_delivered = monthly_UDA_UOAs_delivered * 12 / annual_contracted_UDA_UOA)
+}
+
+
+
+################################################################################
+get_into_slide8_format <- function(data = dental_data_combined, remove_prototypes = F){
+  
+  #remove spaces from column names
+  colnames(data) <- make.names(colnames(data), unique = T)
+  
+  #remove prototype contracts if specified
+  if(remove_prototypes){
+    #create not in function
+    `%notin%` = Negate(`%in%`)
+    data <- data %>%
+      filter(Contract.Number %in% prototype_contracts_orth$proto_contracts)
+  }
+  
+  #group by month and sum UDAs delivered
+  UOAs_delivered <- data %>%
+    group_by(Year.Month) %>%
+    summarise(band1 = sum(FP17s...Band.1, na.rm = T),
+              band2 = sum(FP17s...Band.2, na.rm = T),
+              band3 = sum(FP17s...Band.3, na.rm = T),
+              other = sum(FP17s...Band.Other, na.rm = T),
+              urgent = sum(FP17s...Band.Urgent, na.rm = T)
+              ) %>%
+    rename(month = "Year.Month")
+}
+
+
+################################################################################
+get_slide5_table <- function(data = dental_data_combined, remove_prototypes = F){
+  
+  #remove spaces from column names
+  colnames(data) <- make.names(colnames(data), unique = T)
+  
+  #remove prototype contracts if specified
+  if(remove_prototypes){
+    #create not in function
+    `%notin%` = Negate(`%in%`)
+    data <- data %>%
+      filter(Contract.Number %notin% prototype_contracts$proto_contracts)
+  }
+  
+  #create column for 12 month scaled % of UDAs delivered and put into bands
+  #then sum across these bands by month
+  performance_table <- data %>%
+    mutate(monthly_perc_scaled = UDA.Delivered * 12 * 100/ Annual.contracted.UDA) %>%
+    mutate(performance_band = if_else(monthly_perc_scaled >= 0 & monthly_perc_scaled <10, "0-9%",
+                                      if_else(monthly_perc_scaled >= 10 & monthly_perc_scaled <20, "10-19%",
+                                              if_else(monthly_perc_scaled >= 20 & monthly_perc_scaled <30, "20-29%",
+                                                      if_else(monthly_perc_scaled >= 30 & monthly_perc_scaled <40, "30-39%",
+                                                              if_else(monthly_perc_scaled >= 40 & monthly_perc_scaled <50, "40-49%",
+                                                                      if_else(monthly_perc_scaled >= 50 & monthly_perc_scaled <60, "50-59%",
+                                                                              if_else(monthly_perc_scaled >= 60 & monthly_perc_scaled <70, "60-69%",
+                                                                                      if_else(monthly_perc_scaled >= 70 & monthly_perc_scaled <80, "70-79%",
+                                                                                              if_else(monthly_perc_scaled >= 80 & monthly_perc_scaled <90, "80-89%",
+                                                                                                      if_else( monthly_perc_scaled >= 90 & monthly_perc_scaled <100, "90-99%",
+                                                                                                               "100% +")
+                                                                                              )
+                                                                                      )
+                                                                              )
+                                                                      )
+                                                              )
+                                                      )
+                                              )
+                                      )
+    )
+                                      
+    ) %>%
+    #exclude NAs
+    filter(!is.na(performance_band)) %>%
+    group_by(Year.Month) %>%
+    count(performance_band) %>%
+    mutate(no_of_contracts = sum(n)) %>%
+    ungroup() %>%
+    mutate(perc_contracts_in_band = n * 100 / no_of_contracts)
+
+}
+
+
+################################################################################
+get_slide7_table <- function(data = orthodontic_data_combined, remove_prototypes = F){
+  
+  #remove spaces from column names
+  colnames(data) <- make.names(colnames(data), unique = T)
+  
+  #remove prototype contracts if specified
+  if(remove_prototypes){
+    #create not in function
+    `%notin%` = Negate(`%in%`)
+    data <- data %>%
+      filter(Contract.Number %notin% prototype_contracts$proto_contracts)
+  }
+  
+  #create column for 12 month scaled % of UDAs delivered and put into bands
+  #then sum across these bands by month
+  performance_table <- data %>%
+    mutate(monthly_perc_scaled = UOA * 12 * 100/ Annual.contracted.UOA) %>%
+    mutate(performance_band = if_else(monthly_perc_scaled >= 0 & monthly_perc_scaled <10, "0-9%",
+                                      if_else(monthly_perc_scaled >= 10 & monthly_perc_scaled <20, "10-19%",
+                                              if_else(monthly_perc_scaled >= 20 & monthly_perc_scaled <30, "20-29%",
+                                                      if_else(monthly_perc_scaled >= 30 & monthly_perc_scaled <40, "30-39%",
+                                                              if_else(monthly_perc_scaled >= 40 & monthly_perc_scaled <50, "40-49%",
+                                                                      if_else(monthly_perc_scaled >= 50 & monthly_perc_scaled <60, "50-59%",
+                                                                              if_else(monthly_perc_scaled >= 60 & monthly_perc_scaled <70, "60-69%",
+                                                                                      if_else(monthly_perc_scaled >= 70 & monthly_perc_scaled <80, "70-79%",
+                                                                                              if_else(monthly_perc_scaled >= 80 & monthly_perc_scaled <90, "80-89%",
+                                                                                                      if_else( monthly_perc_scaled >= 90 & monthly_perc_scaled <100, "90-99%",
+                                                                                                               "100% +")
+                                                                                              )
+                                                                                      )
+                                                                              )
+                                                                      )
+                                                              )
+                                                      )
+                                              )
+                                      )
+    )
+    
+    ) %>%
+    #exclude NAs
+    filter(!is.na(performance_band)) %>%
+    group_by(Year.Month) %>%
+    count(performance_band) %>%
+    mutate(no_of_contracts = sum(n)) %>%
+    ungroup() %>%
+    mutate(perc_contracts_in_band = n * 100 / no_of_contracts)
+  
 }
--- functions.R
+++ functions.R
@@ -1,36 +1,37 @@
-#slide 4 and slide 6
-#UDA target percentage by september is 60%
-#UOA target percentage by september is 80%
-format_activity_data <- function(data, septemberTarget){
-  
-  #get data into the right format to plot
-  #annual_contracted_UDA_UOA/2 to get 6 month target
-  data <- data %>%
-    mutate(month = as.Date(month)) %>%
-    mutate(target_UDA_UOAs_delivered_by_sept = (annual_contracted_UDA_UOA/2) * septemberTarget / 100) %>%
-    mutate(perc_of_UDA_UOA_target_delivered = monthly_UDA_UOAs_delivered * 100 / target_UDA_UOAs_delivered_by_sept) %>%
-    mutate(perc_of_UDA_UOA_target_delivered = round(perc_of_UDA_UOA_target_delivered, 1))
-}
+library(tidyverse)
+#plot functions for SMT report and more
+
 
 
 #function to plot first chart on slide 4
-#current data source is in teams folder "Monthly performance data/April 21 to September 21 data/Calendar data/April to Jul UDA 2021-2022 by Treatment month.xlsx"
-plot_UDA_UOA_to_target <- function(data, UDAorUOA){
+#old data source is in teams folder "Monthly performance data/April 21 to September 21 data/Calendar data/April to Jul UDA 2021-2022 by Treatment month.xlsx"
+#pass in raw data - either dental_data_combined_calendar or orthodontic_data_combined_calendar
+plot_UDA_UOA_to_target <- function(data = dental_data_combined_calendar, UDAorUOA = "UDA"){
   
   if(UDAorUOA == "UDA"){
     septemberTarget <- 60
-    title <- "Monthly % of contracted UDAs delivered"
-    ylab <- "% of UDAs delivered"
+    title <- "Monthly % of Apr-Sept target (60%) UDAs delivered"
+    ylab <- "% of target UDAs delivered"
     barCol <- "coral"
+    
+    #get raw data into the right format
+    data <- get_into_slide4_format_calendar(data, remove_prototypes = T)
+    
   }else{
-    septemberTarget = 80
-    title <- "Monthly % of contracted UOAs delivered"
-    ylab <- "% of UOAs delivered"
+    septemberTarget <- 80
+    title <- "Monthly % of Apr-Sept target (80%) UOAs delivered"
+    ylab <- "% of target UOAs delivered"
     barCol <- "seagreen3"
+    
+    #get raw data into fight format
+    data <- get_into_slide6_format_calendar(data, remove_prototypes = F)
   }
   
   #get data in the right format
-  data <- format_activity_data(data, septemberTarget = septemberTarget)
+  data <- data %>%
+    mutate(month = as.Date(month)) %>%
+    mutate(perc_of_UDA_UOA_target_delivered = monthly_UDA_UOAs_delivered * 100 / target_UDA_UOAs_delivered_by_sept) %>%
+    mutate(perc_of_UDA_UOA_target_delivered = round(perc_of_UDA_UOA_target_delivered, 1))
   
   #ensures months with no data are still shown
   #done as separate dataframe so that annotations are not shown for months with no data
@@ -67,25 +68,41 @@ plot_UDA_UOA_to_target <- function(data, UDAorUOA){
          y = ylab)
 }
 
-
+################################################################################
 #function to plot second chart on slide 4
-#current data source is in teams folder "Monthly performance data/April 21 to September 21 data/Calendar data/April to Jul UDA 2021-2022 by Treatment month.xlsx"
-plot_cumulative_UDA_UOA_to_target <- function(data, UDAorUOA){
+#old data source is in teams folder "Monthly performance data/April 21 to September 21 data/Calendar data/April to Jul UDA 2021-2022 by Treatment month.xlsx"
+#pass in raw data - either dental_data_combined_calendar or orthodontic_data_combined_calendar
+plot_cumulative_UDA_UOA_to_target <- function(data = dental_data_combined_calendar, UDAorUOA = "UDA"){
   
   if(UDAorUOA == "UDA"){
-    septemberTarget <- 60
-    title <- "Year to date monthly % of contracted UDAs delivered \ntowards 100% of the 60% threshold"
-    ylab <- "Cumulative % of UDAs delivered \nfrom Apr-21"
+    septemberTarget <- 60 
+    title <- "Year to date monthly % of Apr-Sept target (60%) UDAs delivered"
+    ylab <- "Cumulative % of target UDAs delivered \nfrom Apr-21"
     barCol <- "coral"
+    
+    #get raw data into the right format
+    data <- get_into_slide4_format_calendar(data, remove_prototypes = T)
+    
   }else{
-    septemberTarget = 80
-    title <- "Year to date monthly % of contracted UOAs delivered \ntowards 100% of the 80% threshold"
-    ylab <- "Cumulative % of UOAs delivered \nfrom Apr-21"
+    septemberTarget <- 80
+    title <- "Year to date monthly % of Apr-Sept target (80%) UOAs delivered"
+    ylab <- "Cumulative % of target UOAs delivered \nfrom Apr-21"
     barCol <- "seagreen3"
+    
+    #get raw data into fight format
+    data <- get_into_slide6_format_calendar(data, remove_prototypes = F)
+    
+  }
+  
+  if(nrow(data) < 6){
+    data <- data %>% add_row(month = as.Date("2021-09-01"))
   }
   
   #get data in the right format
-  data <- format_activity_data(data, septemberTarget = septemberTarget)
+  data <- data %>%
+    mutate(month = as.Date(month)) %>%
+    mutate(perc_of_UDA_UOA_target_delivered = monthly_UDA_UOAs_delivered * 100 / target_UDA_UOAs_delivered_by_sept) %>%
+    mutate(perc_of_UDA_UOA_target_delivered = round(perc_of_UDA_UOA_target_delivered, 1))
   
   #cumulative sum column
   data <- data %>%
@@ -139,9 +156,12 @@ plot_cumulative_UDA_UOA_to_target <- function(data, UDAorUOA){
 }
 
 
+
+
+################################################################################
 #function to create graph on slide 8
 #current data source: Dental activity annual running total.xlsx sent by email by Caroline
-plot_banded_CoT <- function(data){
+plot_banded_CoT <- function(data = slide8_data){
   
   #get data into the right format  
   data <- reshape2::melt(data, id.vars = "month")
@@ -168,10 +188,12 @@ plot_banded_CoT <- function(data){
 }
 
 
+
+################################################################################
 #function to create graph on slide 5
 #current data source: Dental activity annual running total.xlsx sent by email by Caroline
 #current data is in teams folder "Monthly performance data\April 21 to September 21 data\Scheduled data\April to Sept UDA 2020-2021 (May).xlsx"
-plot_UDA_UOA_delivery <- function(data, UDAorUOA){
+plot_UDA_UOA_delivery <- function(data = slide5_UDA, UDAorUOA = "UDA"){
   
   if(UDAorUOA == "UDA"){
     title <- "Average percentage of contracted UDAs delivered \nscaled up to 12 months"
@@ -209,9 +231,12 @@ plot_UDA_UOA_delivery <- function(data, UDAorUOA){
 }
 
 
+
+
+################################################################################
 #function to create graph on slide 9
 #current data source: Dental activity annual running total.xlsx sent by email by Caroline
-plot_urgent_form_submissions <- function(data){
+plot_urgent_form_submissions <- function(data = slide8_data){
   
   #get data in the right format
   data <- data %>%
@@ -239,4 +264,115 @@ plot_urgent_form_submissions <- function(data){
          y = "Number of urgent FP17 forms submitted")
     
     
+}
+
+
+################################################################################
+#function to plt delivery profile month by month 
+plot_UDA_delivery_profile <- function(data = UDA_performance_bands){
+  
+  data$performance_band <- factor(data$performance_band, levels = c("0-9%","10% to 19%", 
+                                      "20% to 29%", "30% to 39%", "40% to 49%", "50 to 59%",
+                                      "60% to 69%", "70% to 79%", "80% to 89%", "90% to 99%",
+                                      "100% +"))
+  
+  ggplot(data, 
+         aes(fill = performance_band, 
+             y = perc_of_contracts*100, 
+             x = month)) +
+    geom_bar(position = "dodge", 
+             stat = "identity") +
+    labs(title = "Proportion of contracts delivering in each performance band \nof total contracted UDA per month",
+         x = "Month",
+         y = "Percentage of contracts delivering in this band") +
+    scale_x_datetime(breaks = data$month, 
+                     labels = scales::date_format("%b-%y")) +
+    geom_vline(xintercept = as.Date("2020-07-01"), colour = "black", size = 5)
+}
+
+
+
+
+
+
+
+################################################################################
+#ADJUSTED
+#function to plot first chart on slide 4 adjusted for working day
+#current data source is in teams folder "Monthly performance data/April 21 to September 21 data/Calendar data/April to Jul UDA 2021-2022 by Treatment month.xlsx"
+plot_UDA_UOA_to_target_adjusted <- function(data = get_into_slide4_format_calendar(), UDAorUOA = "UDA"){
+  
+  data <- data %>% mutate(working_day_adjustment = c(0.158730159, 0.150793651, 0.174603175, 0.182539683, 0.158730159)) %>%
+    mutate(adjusted_monthly_target = target_UDA_UOAs_delivered_by_sept * working_day_adjustment) %>%
+    mutate(adjusted_september_target = adjusted_monthly_target * 6)
+  
+  data <- data %>%
+    mutate(month = as.Date(month)) %>%
+    #mutate(target_UDA_UOAs_delivered_by_sept = (annual_contracted_UDA_UOA) * septemberTarget / 100) %>%
+    mutate(perc_of_UDA_UOA_target_delivered = monthly_UDA_UOAs_delivered * 100 / adjusted_september_target) %>%
+    mutate(perc_of_UDA_UOA_target_delivered = round(perc_of_UDA_UOA_target_delivered, 1))
+  
+  if(UDAorUOA == "UDA"){
+    septemberTarget <- 60
+    title <- "Monthly % of Apr-Sept target (60%) UDAs delivered \nAdjusted for working days per month"
+    ylab <- "% of target UDAs delivered"
+    barCol <- "coral"
+  }else{
+    septemberTarget <- 80
+    title <- "Monthly % of Apr-Sept target (80%) UOAs delivered"
+    ylab <- "% of target UOAs delivered"
+    barCol <- "seagreen3"
+  }
+  
+  
+  #ensures months with no data are still shown
+  #done as separate dataframe so that annotations are not shown for months with no data
+  data_to_plot <- data %>%
+    mutate(perc_of_UDA_UOA_target_delivered = if_else(is.na(perc_of_UDA_UOA_target_delivered)
+                                                      , 0, 
+                                                      perc_of_UDA_UOA_target_delivered)) 
+  
+  #plot code
+  ggplot(data_to_plot, 
+         aes(x = month, 
+             y = perc_of_UDA_UOA_target_delivered)) +
+    geom_bar(stat = "identity", 
+             fill = barCol, 
+             width = 10) +
+    theme_bw() +
+    geom_hline(yintercept = 16.7, 
+               colour = "blue", 
+               linetype = "dashed") +
+    annotate(geom = "text", 
+             x = as.Date("2021-09-01"),
+             y = 17.7, 
+             label = "16.7% target", 
+             size = 3) +
+    annotate(geom = "text", 
+             x = data$month, 
+             y = data$perc_of_UDA_UOA_target_delivered + 1, 
+             label = round(data$perc_of_UDA_UOA_target_delivered, 2), 
+             size = 3) +
+    scale_x_date(date_breaks = "1 month", 
+                 date_labels = "%b-%y") +
+    labs(title = title, 
+         x = "Month", 
+         y = ylab)
+}
+
+
+################################################################################
+#unfinished
+get_adjusted_number_reaching_target <- function(){
+  
+  #need to bind in annual contracted UDA from another table
+  
+  dental_data_combined_calendar_contracted4 <- group_by(dental_data_combined_calendar_contracted3, Month)
+  dental_data_combined_calendar_contracted4 <- group_by(dental_data_combined_calendar_contracted4, Contract.Number)
+  dental_data_combined_calendar_contracted5 <- summarise(dental_data_combined_calendar_contracted4)
+  dental_data_combined_calendar_contracted5 <- summarise(dental_data_combined_calendar_contracted4, UDA.Total = sum(UDA.Total), Annual.contracted.UDA = sum(Annual.contracted.UDA), monthly_adjusted_contracted_UDA = sum(monthly_adjusted_contracted_UDA))
+  dental_data_combined_calendar_contracted5 <- mutate(dental_data_combined_calendar_contracted5, perc_adjusted_target_deliverd = UDA.Total * 100 / monthly_adjusted_contracted_UDA)
+  dental_data_combined_calendar_contracted5 <- mutate(dental_data_combined_calendar_contracted5, monthly_contracted_UDA = Annual.contracted.UDA / 12)
+  dental_data_combined_calendar_contracted5 <- mutate(dental_data_combined_calendar_contracted5, perc_contracted_delivered = UDA.Total * 100 / monthly_contracted_UDA)
+  count(dental_data_combined_calendar_contracted5, perc_contracted_delivered >= 50)
 }
