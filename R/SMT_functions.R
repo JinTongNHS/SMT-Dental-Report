e012eeda704a34f46d8ad142c2a28c3223a5fa49
ImogenConnorHelleur <imooconnor@outlook.com>
2021-11-10 18:10:39 +0000

    Included 111 analysis.

--- R/SMT_functions.R
+++ R/SMT_functions.R
@@ -324,7 +324,7 @@ plot_banded_CoT <- function(data = UDA_scheduled_data,
   #plot code
   ggplot(data) +    
     theme_bw() +
-    theme(axis.text.x = element_text(angle = 45)) +
+    theme(axis.text.x = element_text(angle = 90)) +
     geom_line(aes(x = month, 
                   y = CoTs, 
                   colour = band), 
@@ -985,7 +985,93 @@ get_SOF_data <- function(data = UDA_scheduled_data){
   #write.csv(SOF_data, "SOF_data.csv", row.names = F)
 }
   
-    
+
+################################################################################
+plot_111_referrals <- function(data = dental_data_111){
+  
+  data <- data %>% 
+    mutate(month = as.Date(month)) %>%
+    group_by(month) %>%
+    summarise(monthly_case_volume = sum(monthly_case_volume))
+  
+  #plot code
+  ggplot(data) +
+    theme_bw() +
+    theme(axis.text.x = element_text(angle = 90)) +
+    geom_line(aes(x = month, 
+                  y = monthly_case_volume), 
+              colour = "steelblue", 
+              size = 1) +
+    scale_x_date(date_breaks = "1 month", 
+                 date_labels = "%b-%y") +
+    scale_y_continuous(breaks = seq(47000, 
+                                    max(data$monthly_case_volume, na.rm = T) + 10000,
+                                    10000)) +
+    labs(title = "111 call volume recommended towards dental services", 
+         x = "Month",
+         y = "Call volume", 
+         subtitle = "England"#,
+         #caption = "*services include: "
+    ) #+
+  # annotate(geom = "text", 
+  #          x = data$month, 
+  #          y = data$monthly_case_volume + 1, 
+  #          label = data$monthly_case_volume, 
+  #          size = 3) 
   
   
+}
+
+
+################################################################################  
+plot_breakdown_111_referrals <- function(data = dental_data_111){
+  
+  data <- data %>% 
+    mutate(disposition_text = if_else(disposition_text == "Attend Emergency Dental Treatment Centre within 4",
+                                      "Attend Emergency Dental Treatment Centre within 4hrs",
+                                      disposition_text)) %>%
+    mutate(month = as.Date(month)) 
+  
+  #get lines in the right order
+  data$disposition_text <- factor(data$disposition_text,
+                                  levels = c("Attend Emergency Dental Treatment Centre within 4hrs",
+                                             "To Contact a Dental Service within 1 hour",
+                                             "To Contact a Dental Service within 2 hours",
+                                             "Speak to a Dental Service within 2 hours",
+                                             "To Contact a Dental Service within 6 hours",
+                                             "To Contact a Dental Service within 12 hours",
+                                             "To Contact a Dental Service within 24 hours",
+                                             "To Contact a Dental Practice within 5 working days",
+                                             "Dental - Contact Orthodontist next working day"
+                                             ))
+  
+  #plot code
+  ggplot(data) +
+    theme_bw() +
+    theme(axis.text.x = element_text(angle = 90)) +
+    geom_line(aes(x = month, 
+                  y = monthly_case_volume,
+                  colour = disposition_text), 
+              size = 1) +
+    scale_x_date(date_breaks = "1 month", 
+                 date_labels = "%b-%y") +
+    # scale_y_continuous(breaks = seq(47000, 
+    #                                 max(data$monthly_case_volume, na.rm = T) + 10000,
+    #                                 10000)) +
+    labs(title = "111 call volume recommended towards dental services", 
+         x = "Month",
+         y = "Call volume", 
+         subtitle = "England",
+         colour = "Disposition"#,
+         #caption = "*services include: "
+    ) #+
+  # annotate(geom = "text", 
+  #          x = data$month, 
+  #          y = data$monthly_case_volume + 1, 
+  #          label = data$monthly_case_volume, 
+  #          size = 3) 
+  
+  
+}
+
   
