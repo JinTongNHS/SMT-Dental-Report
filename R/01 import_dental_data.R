ed7fb11de50b1eeae1cd2785bae134484a6a0fa2
ImogenConnorHelleur <imooconnor@outlook.com>
2022-06-01 15:56:44 +0100

    New function for SQL upload.

--- R/01 import_dental_data.R
+++ R/01 import_dental_data.R
@@ -1,5 +1,105 @@
 library(tidyverse)
 library(readxl)
+library(DBI)
+library(odbc)
+
+################################################################################
+import_clean_combine_upload_all_data <- function(raw_UDA_scheduled_data_folder_path = "N:/_Everyone/Primary Care Group/SMT_Dental/raw_eDEN_dental_data/UDA_scheduled_raw_data/",
+                                             raw_UOA_scheduled_data_folder_path = "N:/_Everyone/Primary Care Group/SMT_Dental/raw_eDEN_dental_data/UOA_scheduled_raw_data/",
+                                             raw_UDA_calendar_data_folder_path = "N:/_Everyone/Primary Care Group/SMT_Dental/raw_eDEN_dental_data/UDA_calendar_raw_data/",
+                                             raw_UOA_calendar_data_folder_path = "N:/_Everyone/Primary Care Group/SMT_Dental/raw_eDEN_dental_data/UOA_calendar_raw_data/"){
+
+  # Gets last month 
+  data_month <- lubridate::floor_date(Sys.Date()  - lubridate::weeks(4), unit = "month")
+  data_month_name <- format(Sys.Date()  - lubridate::weeks(4), format = "%b%y")
+  
+  #UDA scheduled data
+  UDA_scheduled_data_latest <- import_and_clean_scheduled_UDA_data(data_path = paste0(raw_UDA_scheduled_data_folder_path, "UDA_scheduled_", data_month_name,".xlsx"),
+                                                                 data_date = data_month)
+
+  #UOA scheduled data
+  UOA_scheduled_data_latest <- import_and_clean_scheduled_UOA_data(data_path = paste0(raw_UOA_scheduled_data_folder_path, "UOA_scheduled_", data_month_name,".xlsx"),
+                                                                   data_date = data_month)
+
+  #UDA calendar data
+  UDA_calendar_data_latest <- import_and_clean_calendar_UDA_data(data_path = paste0(raw_UDA_calendar_data_folder_path, "UDA_calendar_Apr_", data_month_name,".xlsx"))
+  
+  #UOA calendar data
+  UOA_calendar_data_latest <- import_and_clean_calendar_UOA_data(data_path = paste0(raw_UOA_calendar_data_folder_path, "UOA_calendar_Apr_", data_month_name,".xlsx"))
+  
+  #Append schedule data and overwrite calendar data in NCDR
+  con <- dbConnect(odbc::odbc(), "NCDR")
+  
+  #Append UDA scheduled data
+  dbWriteTable(con, Id(catalog="NHSE_Sandbox_PrimaryCareNHSContracts",schema="Dental",table="UDA_scheduled"),
+               value = UDA_scheduled_data_latest, row.names = FALSE, append=TRUE)
+
+  #Append UOA scheduled data
+  dbWriteTable(con, Id(catalog="NHSE_Sandbox_PrimaryCareNHSContracts",schema="Dental",table="UOA_scheduled"),
+               value = UOA_scheduled_data_latest, row.names = FALSE, append=TRUE)
+
+  #Get UDA calendar table and overwrite this financial year
+  sql <- "SELECT [data_month]
+      ,[contract_number]
+      ,[latest_contract_type]
+      ,[name_or_company_name]
+      ,[commissioner_name]
+      ,[region_name]
+      ,[paid_by_BSA]
+      ,[contract_start_date]
+      ,[contract_end_date]
+      ,[UDA_total]
+      ,[UDA_band_1_total]
+      ,[UDA_band_2_total]
+      ,[UDA_band_3_total]
+      ,[UDA_urgent_total]
+      ,[UDA_other_total]
+      ,[total_FP17s]
+      ,[total_band_1_FP17s]
+      ,[total_band_2_FP17s]
+      ,[total_band_3_FP17s]
+      ,[total_urgent_FP17s]
+      ,[total_other_FP17s]
+  FROM [NHSE_Sandbox_PrimaryCareNHSContracts].[Dental].[UDA_calendar]"
+  result <- dbSendQuery(con, sql)
+  UDA_calendar_data <- dbFetch(result)
+  dbClearResult(result)
+
+  UDA_calendar_data_latest <- UDA_calendar_data %>%
+    filter(data_month < as.Date("2022-04-01")) %>%
+    bind_rows(UDA_calendar_data_latest)
+
+  dbWriteTable(con, Id(catalog="NHSE_Sandbox_PrimaryCareNHSContracts",schema="Dental",table="UDA_calendar"),
+               value = UDA_calendar_data_latest, row.names = FALSE, append = FALSE, overwrite = TRUE)
+
+
+  #Get UOA table and overwrite this financial year
+  sql <- "SELECT [data_month]
+      ,[contract_number]
+      ,[contract_type]
+      ,[name_or_company_name]
+      ,[commissioner_name]
+      ,[paid_by_BSA]
+      ,[contract_start_date]
+      ,[contract_end_date]
+      ,[UOA_total]
+  FROM [NHSE_Sandbox_PrimaryCareNHSContracts].[Dental].[UOA_calendar]"
+
+  result <- dbSendQuery(con, sql)
+  UOA_calendar_data <- dbFetch(result)
+  dbClearResult(result)
+
+  UOA_calendar_data_latest <- UOA_calendar_data %>%
+    filter(data_month < as.Date("2022-04-01")) %>%
+    bind_rows(UOA_calendar_data_latest)
+
+  dbWriteTable(con, Id(catalog="NHSE_Sandbox_PrimaryCareNHSContracts",schema="Dental",table="UOA_calendar"),
+               value = UOA_calendar_data_latest, row.names = FALSE, append = FALSE, overwrite = TRUE)
+  
+
+}
+
+
 
 ################################################################################
 #function to import and clean data
@@ -87,9 +187,7 @@ import_and_clean_scheduled_UDA_data <- function(data_path = "data/raw_data/dashb
            FP17s_urgent_prev_2_year = X__45, #remove
            FP17s_other_prev_2_year = X__46  #remove
     ) %>%
-    select(-UDA_financial_half_target) #%>%
-    #incorporate target change
-    #mutate(UDA_financial_half_target = if_else(data_month >= as.Date("2021-10-01"), annual_contracted_UDA * 0.65 /2, UDA_financial_half_target))
+    select(-UDA_financial_half_target) 
   
 }
 
@@ -138,10 +236,36 @@ import_and_clean_calendar_UDA_data <- function(data_path = "data/raw_data/dashbo
            total_other_FP17s = X__20
     ) 
   
+  #add column for date and rename columns, split data for just may
+  data_may <- data %>%
+    mutate(data_month = as.Date("2022-05-01")) %>%
+    select(data_month, X__1, X__2, X__3, X__4, X__5, X__6, X__7, X__8, 
+           X__21, X__22, X__23, X__24, X__25, X__26, X__27, X__28, X__29, X__30, X__31, X__32) %>%
+    rename(contract_number = X__1,
+           latest_contract_type = X__2,
+           name_or_company_name = X__3,
+           commissioner_name = X__4,
+           region_name = X__5,
+           paid_by_BSA = X__6,
+           contract_start_date = X__7, 
+           contract_end_date = X__8, 
+           
+           UDA_total = X__21, 
+           UDA_band_1_total = X__22,
+           UDA_band_2_total = X__23,
+           UDA_band_3_total = X__24, 
+           UDA_urgent_total = X__25, 
+           UDA_other_total = X__26, 
+           total_FP17s = X__27, 
+           total_band_1_FP17s  = X__28,
+           total_band_2_FP17s = X__29,
+           total_band_3_FP17s = X__30,
+           total_urgent_FP17s = X__31,
+           total_other_FP17s = X__32
+    ) 
   
-  # UDA_calendar_data <- bind_rows(data_apr, data_may, data_jun, data_jul, data_aug, 
-  #                                data_sep, data_oct, data_nov, data_dec, data_jan,
-  #                                data_feb, data_mar)
+  
+  UDA_calendar_data <- bind_rows(data_apr, data_may)
   
 }
 
@@ -181,11 +305,25 @@ import_and_clean_calendar_UOA_data <- function(data_path = "data/raw_data/dashbo
            contract_end_date = X__7, 
            
            UOA_total = X__8
-    ) 
+    )
   
-  # UOA_calendar_data <- bind_rows(data_apr, data_may, data_jun, data_jul, data_aug, 
-  #                                data_sep, data_oct, data_nov, data_dec, data_jan,
-  #                                data_feb, data_mar)
+  #add column for date and rename columns, split data for just may
+  data_may <- data %>%
+    mutate(data_month = as.Date("2022-05-01")) %>%
+    select(data_month, X__1, X__2, X__3, X__4, X__5, X__6, X__7, 
+           X__9) %>%
+    rename(contract_number = X__1,
+           contract_type = X__2,
+           name_or_company_name = X__3,
+           commissioner_name = X__4,
+           paid_by_BSA = X__5,
+           contract_start_date = X__6, 
+           contract_end_date = X__7, 
+           
+           UOA_total = X__9
+    )
+  
+  UOA_calendar_data <- bind_rows(data_apr, data_may)
   
 }
 
@@ -208,6 +346,9 @@ import_and_clean_scheduled_UOA_data <- function(data_path = "data/raw_data/dashb
                      skip = 3,
                      col_names = TRUE,
                      .name_repair = ~ paste0("X__", seq_along(.x)))
+  
+  #remove last 4 rows with eDen source
+  data <- data[1:(nrow(data) - 4),]
   
   #check format of data - manual check should be done to see if columns are in the same order as expected
   if(ncol(data) != 18){
