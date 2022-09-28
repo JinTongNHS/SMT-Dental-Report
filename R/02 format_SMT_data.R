5fdd7558cd43bdeeb651d5a624f0d2258c92d794
ImogenConnorHelleur <imooconnor@outlook.com>
2022-09-28 08:20:47 +0100

    Adds function to pull in PCDID data.

--- R/02 format_SMT_data.R
+++ R/02 format_SMT_data.R
@@ -1,4 +1,18 @@
 library(tidyverse)
+#[NHSE_Sandbox_PrimaryCareNHSContracts].[Dental].[UDA_delivery_scheduled]
+################################################################################
+pull_PCDID_data <- function(){
+  
+  con <- dbConnect(odbc::odbc(), "NCDR")
+  
+  sql <- "SELECT *
+  FROM [NHSE_Sandbox_PrimaryCareNHSContracts].[Dental].[UDA_delivery_scheduled]"
+  result <- dbSendQuery(con, sql)
+  UDA_calendar_data <- dbFetch(result)
+  dbClearResult(result)
+  
+  UDA_calendar_data
+}
 
 ################################################################################
 pull_UDA_calendar_data <- function(){
