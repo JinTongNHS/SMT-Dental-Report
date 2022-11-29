07868a1e3f1310d5ed48b4cb2574cc1d2bc852d3
Emran Mohammed <MEmran@GEMCSU.NHS.UK>
2022-11-29 13:09:43 +0000

    edited legends in plot 1

--- R/03 plot_functions/DCP_analysis.R
+++ R/03 plot_functions/DCP_analysis.R
@@ -2,47 +2,50 @@
 
 library(tidyverse)
 library(ggpubr)
+library("scales")
+library(formattable)
 
 ##### Import Data
 
-dpc_main <- read.csv("N:/_Everyone/Primary Care Group/SMT_Dental DENT 2022_23-008/DCP_data/DPC_v1_Oct_2022.csv") 
+dcp_main <- read.csv("N:/_Everyone/Primary Care Group/SMT_Dental DENT 2022_23-008/DCP_data/DPC_v1_Oct_2022.csv") 
 
 
 ###UDA Chart
-UDA_plot_all_t <- dpc_main %>%
-  group_by(DCP_description) %>%
-  summarise (c_total_B1 = sum(Band_1._UDA, na.rm = TRUE),
-             c_total_B2 = sum(Band_2._UDA, na.rm = TRUE),
-             c_total_B3 = sum(Band_3._UDA, na.rm = TRUE),
-             c_total_urgent = sum(Urgent_UDA, na.rm = TRUE)) %>%
-             mutate (ratio_total_B1 = c_total_B1 / (c_total_B1 + c_total_B2 + c_total_B3 +c_total_urgent),
-                    label = percent(ratio_total_B1 %>% round(2)), 
-                    new_o = " (",
-                    new_c = ")",
-                    total_B1 = paste (c_total_B1, new_o, label, new_c, sep = '')) %>%
-  
-            mutate (ratio_total_B2 = c_total_B2 / (c_total_B1 + c_total_B2 + c_total_B3 + c_total_urgent),
-            label_2 = percent(ratio_total_B2 %>% round(2)), 
-            new_o_2 = " (",
-            new_c_2 = ")",
-            total_B2 = paste (c_total_B2, new_o_2, label_2, new_c_2, sep = ''))%>%
-  
-  
-          mutate (ratio_total_B3 = c_total_B3 / (c_total_B1 + c_total_B2 + c_total_B3 +c_total_urgent),
-          label_3 = percent(ratio_total_B3 %>% round(2)), 
-          new_o_3 = " (",
-          new_c_3 = ")",
-          total_B3 = paste (c_total_B3, new_o_3, label_3, new_c_3, sep = '')) %>%
-    
-    mutate (ratio_total_urgent = c_total_urgent / (c_total_B1 + c_total_B2 + c_total_B3 +c_total_urgent),
-          label_urgent = percent(ratio_total_urgent %>% round(2)), 
-          new_o_urgent = " (",
-          new_c_urgent = ")",
-          total_urgent = paste (c_total_urgent, new_o_urgent, label_urgent, new_c_urgent, sep = '')) %>%
-    select (DCP_description, total_B1, total_B2, total_B3, total_urgent )
+test_1 <- dcp_main %>%
+  group_by(DCP_description, ) %>%
+  summarise (B1 = sum(Band_1._UDA, na.rm = TRUE),
+             B2 = sum(Band_2._UDA, na.rm = TRUE),
+             B3 = sum(Band_3._UDA, na.rm = TRUE),
+             urgent = sum(Urgent_UDA, na.rm = TRUE))
+
+
+data_long <- pivot_longer(test_1, cols = -DCP_description, 
+                          names_to = 'Total_UDA_Each_Band', 
+                          values_to = 'UDAs')
+plot_test_1 <- group_by(data_long, DCP_description) %>% 
+  mutate(percent = formattable::percent (UDAs/sum(UDAs)))
+
+d_test <-
+  plot_test_1 %>%
+  mutate(new_o = " (",
+         new_c = ")",
+         label_n = paste (UDAs, new_o, percent, new_c, sep = ''))
+
+plot_1 <- d_test %>%  ggplot(aes(fill = Total_UDA_Each_Band , y = UDAs, x = DCP_description)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  geom_text(aes(label = label_n), ###paste0(UDAs, sep="~", percent)),
+            colour = "black",
+            position = position_dodge(width = .9)) +
+  theme(legend.position="bottom") +
+  coord_flip() +
+  ggtitle("UDAs Delivered by DCPs in October 2022") +
+  theme(axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank())
+
 
 ###FP17 Chart
-FP17_plot_all_t <- dpc_main %>%
+FP17_plot_all_t <- dcp_main %>%
   group_by(DCP_description) %>%
   summarise (c_total_FP17 = sum(FP17_Current_Year_total, na.rm = TRUE)) %>%
   mutate (FP17_percent = sum(c_total_FP17), 
@@ -51,21 +54,7 @@ FP17_plot_all_t <- dpc_main %>%
           new_o_FP17 = " (",
           new_c_FP17 = ")",
           total_FP17 = paste (c_total_FP17, new_o_FP17, label_FP17, new_c_FP17, sep = '')) %>%
-          select (DCP_description, total_FP17) 
-
-
-
-plot_1<- UDA_plot_all_t %>% pivot_longer(-DCP_description, names_to = "UDAs", values_to = "total_percent_each_DCP")%>%
-  group_by(DCP_description) %>% 
-  ggplot(., aes(fill = UDAs, y = reorder (UDAs, desc(total_percent_each_DCP)), x = DCP_description)) +
-  geom_bar(position = "dodge", stat = "identity") +
-  geom_text (aes(label = total_percent_each_DCP), position = position_dodge(width = .8)) +
-  coord_flip() +
-  ggtitle("UDAs Delivered by DCPs in October 2022") + 
-  theme(axis.title.x=element_blank(),
-        axis.text.x=element_blank(),
-        axis.ticks.x=element_blank())
-
+  select (DCP_description, total_FP17) 
 
 plot_2 <- FP17_plot_all_t %>% pivot_longer(-DCP_description, names_to = "FP17", values_to = "total_percent")%>%
   group_by(DCP_description) %>% 
@@ -74,6 +63,7 @@ plot_2 <- FP17_plot_all_t %>% pivot_longer(-DCP_description, names_to = "FP17", 
   geom_text (aes(label = total_percent), position = position_dodge(width = .8)) +
   coord_flip() +
   ggtitle("FP17 Delivered by DCPs in October 2022") +
+  theme(legend.position="bottom") +
   theme(axis.title.x=element_blank(),
         axis.text.x=element_blank(),
         axis.ticks.x=element_blank(),
