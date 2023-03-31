608a32e80d90e271d170058bab3b342efe814d4d
ImogenConnorHelleur <imooconnor@outlook.com>
2023-03-31 11:41:40 +0100

    Changed file and function name. Neatened aesthetics.

--- R/03 plot_functions/plot_number_new_patients.R
+++ R/03 plot_functions/plot_number_new_patients.R
@@ -1,42 +1,42 @@
-library (tidyverse)
-library(DBI)
-library(odbc)
-library(formattable)
+# library (tidyverse)
+# library(DBI)
+# library(odbc)
+# library(formattable)
+# 
+# ################################################################################
+# pull_UDA_scheduled_data <- function(){
+#   
+#   con <- dbConnect(odbc::odbc(), "NCDR")
+#   
+#   sql <- "SELECT *
+#   FROM [NHSE_Sandbox_PrimaryCareNHSContracts].[Dental].[UDA_scheduled]"
+#   result <- dbSendQuery(con, sql)
+#   UDA_scheduled_data <- dbFetch(result)
+#   dbClearResult(result)
+#   
+#   UDA_scheduled_data %>%
+#   rename( month = data_month) 
+# }
+# 
+# pull_new_vs_returning_patients <- function(){
+#   
+#   con <- dbConnect(odbc::odbc(), "NCDR")
+#   sql <- "SELECT * FROM [NHSE_Sandbox_PrimaryCareNHSContracts].[Dental].[New_vs_Returning_Patients]"
+#   
+#   result <- dbSendQuery (con, sql)
+#   new_vs_return <- dbFetch(result)
+#   dbClearResult(result)
+#   new_vs_return
+# }
+# ###UDA data
+# 
+# ##colnames(new_vs_return_data)
+# 
+# new_vs_return_data <- pull_new_vs_returning_patients()
+# 
+# UDA_scheduled_data <-pull_UDA_scheduled_data()
 
-################################################################################
-pull_UDA_scheduled_data <- function(){
-  
-  con <- dbConnect(odbc::odbc(), "NCDR")
-  
-  sql <- "SELECT *
-  FROM [NHSE_Sandbox_PrimaryCareNHSContracts].[Dental].[UDA_scheduled]"
-  result <- dbSendQuery(con, sql)
-  UDA_scheduled_data <- dbFetch(result)
-  dbClearResult(result)
-  
-  UDA_scheduled_data %>%
-  rename( month = data_month) 
-}
-
-pull_new_vs_returning_patients <- function(){
-  
-  con <- dbConnect(odbc::odbc(), "NCDR")
-  sql <- "SELECT * FROM [NHSE_Sandbox_PrimaryCareNHSContracts].[Dental].[New_vs_Returning_Patients]"
-  
-  result <- dbSendQuery (con, sql)
-  new_vs_return <- dbFetch(result)
-  dbClearResult(result)
-  new_vs_return
-}
-###UDA data
-
-##colnames(new_vs_return_data)
-
-new_vs_return_data <- pull_new_vs_returning_patients()
-
-UDA_scheduled_data <-pull_UDA_scheduled_data()
-
-plot_number_new_vs_returning_patients <- function(data = new_vs_return_data,
+plot_number_new_patients <- function(data = new_vs_return_data,
                                                   level = "National",
                                                   stp_region_name = NULL, 
                                                   scheduled_data = UDA_scheduled_data,
@@ -71,9 +71,9 @@ plot_number_new_vs_returning_patients <- function(data = new_vs_return_data,
   # scheduled_data <- scheduled_data %>% filter(data_month >= as.Date("2022-04-01")) 
   # 
   #  
-  # contractors_number_all_patients <- scheduled_data %>% 
-  #   group_by(data_month) %>%
-  #   dplyr::count(data_month, name = "total_number_of_contractors_submitted_FP17")
+  contractors_number_all_patients <- scheduled_data %>%
+    group_by(month) %>%
+    dplyr::count(month, name = "total_number_of_contractors_submitted_FP17")
   # 
   contractors_number_england <- scheduled_data %>%
     dplyr::count(month, name = "total_number_of_contractors_submitted_FP17")
@@ -133,17 +133,24 @@ plot_number_new_vs_returning_patients <- function(data = new_vs_return_data,
                                              aes(x = month, y = number,
                                             group =category )) +
       geom_line(aes(color=category),
-                linewidth = 1.2)+
+                size = 1.2)+
       geom_point(aes(color=category),
                  size = 3) +
       scale_x_date(date_labels = "%b-%Y", breaks = "1 month") +
+      scale_y_continuous(breaks = scales::breaks_pretty(),
+                         labels=function(x) format(x, big.mark = ",", scientific = FALSE)) +
+      scale_colour_manual(values = c("steelblue", "coral"), labels = c("New adult patients", "New child patients")) +
       expand_limits(y=0) +
       geom_text(aes(label = number), vjust=-.5)+
-      theme_classic() +
+      theme_bw() +
       ##theme(legend.position="bottom") +
       theme(legend.position="top") +
-      labs(title = "New Patient (no previous in last 24 months or before) Numbers",
-           subtitle = subtitle)
+      labs(title = "Number of New Patients (no previous in last 24 months or before)",
+           x = "Month",
+           y = "Number of new patients",
+           colour = "Category",
+           subtitle = subtitle) +
+      theme(axis.text.x = element_text(angle = 90, vjust=-0.0001))
     
     new_patient_line_chart_england
     
@@ -158,7 +165,7 @@ plot_number_new_vs_returning_patients <- function(data = new_vs_return_data,
                 served_new_adult_patients = sum(!is.na(adult))) 
     
     all_provider <- left_join(new_patients_provider_number, 
-                              contractors_number_all_patients, by = c("month" = "data_month"))
+                              contractors_number_all_patients, by = c("month"))
     
     
     all_provider <- all_provider %>% 
@@ -180,17 +187,22 @@ plot_number_new_vs_returning_patients <- function(data = new_vs_return_data,
                                          aes(x = month, y = Percentage,
                                              group =category )) +
       geom_line(aes(color=category),
-                linewidth = 1.5)+
+                size = 1.2)+
       geom_point(aes(color=category),
                  size = 3) +
      scale_x_date(date_labels = "%b-%Y", breaks = "1 month") +
+      scale_y_continuous(breaks = seq(0, 1.1, 0.2),
+                         limits = c(0, 1.1),
+                         labels = scales::percent_format(accuracy = 1)) +
       expand_limits(y=0) +
       geom_text(aes(label = Percentage), vjust=-.5)+
-      theme_classic() +
+      theme_bw() +
       ##theme(legend.position="bottom") +
       theme(legend.position="top") +
-      labs(title = "% of Providers Served New Patients",
-           subtitle = subtitle)
+      labs(title = "Percentage of Providers Serving New Patients",
+           x = "Month",
+           subtitle = subtitle) +
+      theme(axis.text.x = element_text(angle = 90, vjust=-0.0001))
     
     provider_chart 
     
