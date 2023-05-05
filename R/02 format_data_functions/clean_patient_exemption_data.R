ef0540b5cd2f12a58ea2d62a878c5d66d4361d23
ImogenConnorHelleur <imooconnor@outlook.com>
2023-05-05 09:28:00 +0100

    Fixes bug with cleaning patient expemption data.

--- R/02 format_data_functions/clean_patient_exemption_data.R
+++ R/02 format_data_functions/clean_patient_exemption_data.R
@@ -1,7 +1,7 @@
 clean_patient_exemption_data <- function(data = patient_exemption_data){
   
   data_long <- gather(data, patient_type, number_uda, ###best to use pivot_longer
-                      Child:adult_non_exempt, 
+                      Child, adult_non_exempt, 
                       factor_key=TRUE)
   
   
