a35b8d0e468fb3617125f3656432cce3a84747a5
ImogenConnorHelleur <imooconnor@outlook.com>
2023-05-11 11:28:17 +0100

    Fixes issue with clean_patient_exemption_data.

--- R/02 format_data_functions/clean_patient_exemption_data.R
+++ R/02 format_data_functions/clean_patient_exemption_data.R
@@ -1,10 +1,12 @@
 clean_patient_exemption_data <- function(data = patient_exemption_data){
   
-  data_long <- gather(data, patient_type, number_uda, ###best to use pivot_longer
-                      Child, adult_non_exempt, 
+  data_long <- gather(data, 
+                      key = patient_type, 
+                      value = number_uda, 
+                      -Treatment_Charge_Band, -Regions, -FY, -adult_exempt,
+                      #-Child, -adult_non_exempt, 
                       factor_key=TRUE)
-  
-  
+
   ###to remove comma's from data column
   data_long <- data_long %>%
     mutate(number_uda = str_replace(number_uda, ",", "")) %>%
