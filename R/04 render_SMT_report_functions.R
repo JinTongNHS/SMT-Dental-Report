850ad6ca52d6f643e56ea90dcc0b9087c9888628
ImogenConnorHelleur <imooconnor@outlook.com>
2023-06-06 15:40:58 +0100

    Changed file names ready for next month's refresh.

--- R/04 render_SMT_report_functions.R
+++ R/04 render_SMT_report_functions.R
@@ -5,7 +5,7 @@ render_report <- function(region) {
   rmarkdown::render(
     "rmarkdown/SMT_dental_report_region_ICB_level.rmd", params = list(
       region = region),
-    output_file = paste0("05_2023_SMT_Dental_Pack_", gsub(" ", "", region),"_ICBs_reporting_up_to_end_of_April_2023.html")
+    output_file = paste0("07_2023_SMT_Dental_Pack_", gsub(" ", "", region),"_ICBs_reporting_up_to_end_of_June_2023.html")
   )
 }
 
