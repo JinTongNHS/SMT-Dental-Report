d7f66bf1ba5371d75d617226048d34ca0e04ed97
ImogenConnorHelleur <imooconnor@outlook.com>
2023-06-06 15:39:43 +0100

    Adds in extra data pulls so that metric tables update.

--- R/01 import_functions/03 run_upload_functions.R
+++ R/01 import_functions/03 run_upload_functions.R
@@ -48,8 +48,75 @@ upload_unique_patients_data(unique_patients = unique_patients_latest)
 #upload band 2 split data
 upload_band2_split_data(band2_split = band2_split_latest)
 
-###### To be run after the rmarkdown is run ####################################
-# #Upload metrics
+################################################################################
+# Pulls in new data before metric tables can be uploaded
+
+            library(tidyverse)
+            library(readxl)
+            library(DBI)
+            library(odbc)
+            library(reactable) #make sure you have the latest version by doing install.packages("reactable")
+            library(downloadthis)
+            library(lubridate)
+            
+            #source in all file from plot folder
+            for (f in list.files(path = "../R/03 plot_functions/", pattern="*.R$")) {
+              source(paste0("../R/03 plot_functions/", f))
+            }
+            
+            for (f in list.files(path = "../R/02 format_data_functions/", pattern="*.R$")) {
+              source(paste0("../R/02 format_data_functions/", f))
+            }
+            
+            
+            #pull data from NCDR
+            UDA_calendar_data <- pull_UDA_calendar_data()
+            UOA_calendar_data <- pull_UOA_calendar_data()
+            UDA_scheduled_data <- pull_UDA_scheduled_data()
+            UOA_scheduled_data <- pull_UOA_scheduled_data()
+            payments_to_dentists <- pull_payments_to_dentist()
+            
+            historical_UDA_scheduled_data <- pull_UDA_scheduled_historical_data()
+            historical_UDA_scheduled_data <- rename(historical_UDA_scheduled_data, month = data_month)
+            
+            historical_UOA_scheduled_data <- pull_UOA_scheduled_historical_data()
+            historical_UOA_scheduled_data <- rename(historical_UOA_scheduled_data, month = data_month)
+            
+            #change column names and add regions
+            UDA_calendar_data <- rename(UDA_calendar_data, month = data_month)
+            UOA_calendar_data <- UOA_calendar_data %>%
+              rename(month = data_month) %>%
+              mutate(contract_number = as.numeric(contract_number),
+                     UOA_total = as.numeric(UOA_total))
+            
+            
+            # UOA_calendar_data <- left_join( region_STP_lookup, by = c("contract_number", "name_or_company_name", "commissioner_name"))
+            UDA_scheduled_data <- rename(UDA_scheduled_data, month = data_month)
+            UOA_scheduled_data <- rename(UOA_scheduled_data, month = data_month)
+            
+            UDA_scheduled_data <- mutate(UDA_scheduled_data, month = as.Date(month))
+            UOA_scheduled_data <- mutate(UOA_scheduled_data, month = as.Date(month))
+            
+            #sort out STP ICB changes
+            STP_ICB_lookup_codes <- read_excel("N:/_Everyone/Primary Care Group/SMT_Dental DENT 2022_23-008/data_for_monthly_report/STP_ICB_lookup_codes.xlsx")
+            
+            #Read local 111 file
+            dental_data_111 <- read_excel("N:/_Everyone/Primary Care Group/SMT_Dental DENT 2022_23-008/111_pathways_data/dental_data_111.xlsx")
+            
+            #############still to fix
+            prototype_contracts <- read_excel("N:/_Everyone/Primary Care Group/SMT_Dental DENT 2022_23-008/data_for_monthly_report/prototype_contracts.xlsx")
+            contractor_categories <- read_excel("N:/_Everyone/Primary Care Group/SMT_Dental DENT 2022_23-008/data_for_monthly_report/contractor_categories_Jan23.xlsx")
+            contractor_categories <- contractor_categories %>%
+              mutate(contract_number = as.numeric(contract_number))
+            contract_demographics <- readRDS("N:/_Everyone/Primary Care Group/SMT_Dental DENT 2022_23-008/data_for_monthly_report/contract_demographics.rds")
+            
+            #read in unique patients
+            #unique_patients_rolling <- readRDS("../data/unique_patients/unique_patients_rolling.rds")
+            unique_patients_rolling <- pull_unique_patients_data()
+
+
+
+
 upload_delivery_metrics()
 update_SOF_table_S109()
 upload_unique_patients_metric()
