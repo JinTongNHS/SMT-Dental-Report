c728925dbb5533678b7d965d242a72d26589ac52
Emran Mohammed <MEmran@GEMCSU.NHS.UK>
2023-07-28 10:00:58 +0100

    change in the plots as per suggestion of caroline

--- R/03 plot_functions/plot_banded_COT_treatment_month_v3.R
+++ R/03 plot_functions/plot_banded_COT_treatment_month_v3.R
@@ -1,5 +1,5 @@
-##need to uncomment
-
+# ##Aneed to uncomment
+# 
 # #get historic data ready
 # historic_data <- historical_UDA_scheduled_data  %>%
 #   mutate(financial_year = case_when(month >= as.Date("2016-04-01") & month < as.Date("2017-04-01") ~ "2016/17",
@@ -14,12 +14,12 @@
 #   group_by(financial_year) %>%
 #   summarise(full_year_UDA_delivery = sum(UDA_delivered, na.rm = TRUE),
 #             full_year_contracted_UDAs = sum(annual_contracted_UDA, na.rm = TRUE) / 12) %>%
-#   mutate(perc_UDA_deliverd = full_year_UDA_delivery * 100 / full_year_contracted_UDAs) %>% 
+#   mutate(perc_UDA_deliverd = full_year_UDA_delivery * 100 / full_year_contracted_UDAs) %>%
 #   select(financial_year, full_year_UDA_delivery)
 # 
 # 
 # 
-# UDA_scheduled_data_time_series<- UDA_scheduled_data %>% 
+# UDA_scheduled_data_time_series<- UDA_scheduled_data %>%
 #   mutate(financial_year = case_when(month >= as.Date("2016-04-01") & month < as.Date("2017-04-01") ~ "2016/17",
 #                                     month >= as.Date("2017-04-01") & month < as.Date("2018-04-01") ~ "2017/18",
 #                                     month >= as.Date("2018-04-01") & month < as.Date("2019-04-01") ~ "2018/19",
@@ -29,16 +29,16 @@
 #                                     month >= as.Date("2022-04-01") & month < as.Date("2023-04-01") ~ "2022/23",
 #                                     month >= as.Date("2023-04-01") & month < as.Date("2024-04-01") ~ "2023/24",
 #                                     month >= as.Date("2024-04-01") & month < as.Date("2025-04-01") ~ "2024/25")) %>%
-#   ##filter(financial_year !="2023/24") %>% 
-#   group_by(financial_year) %>% 
+#   ##filter(financial_year !="2023/24") %>%
+#   group_by(financial_year) %>%
 #   summarise(full_year_UDA_delivery = sum(UDA_delivered, na.rm = TRUE))
 # 
 # a<- max(UDA_scheduled_data_time_series$financial_year)
 # 
-# year_end_data_for_plot <- historic_data %>% 
-#   rbind(UDA_scheduled_data_time_series) %>% 
-#   mutate(year_type = if_else(financial_year == a, 
-#                              "Current Financial Year", 
+# year_end_data_for_plot <- historic_data %>%
+#   rbind(UDA_scheduled_data_time_series) %>%
+#   mutate(year_type = if_else(financial_year == a,
+#                              "Current Financial Year",
 #                              "Previous Financial Years")) %>%
 #   filter((substr(month, 6, 7) == "04" & substr(month, 9, 10) == "01")| year_type == "Current Financial Year")
 # 
@@ -74,13 +74,13 @@
 #   group_by(financial_year) %>%
 #   summarise(full_year_UDA_delivery = sum(UDA_delivered, na.rm = TRUE))
 # 
-# historic_data <- historic_data_1 %>% 
+# historic_data <- historic_data_1 %>%
 #   rbind(historic_data_2)
 # 
 # #get treatment month data ready
 # data <- UDA_treatment_month_non_FD %>%
 #   group_by(month) %>%
-#   summarise(UDA_delivery = sum(UDA_delivered, na.rm = TRUE)) %>% 
+#   summarise(UDA_delivery = sum(UDA_delivered, na.rm = TRUE)) %>%
 #             ##contracted_UDAs = sum(annual_contracted_UDA, na.rm = TRUE) / 12) %>%
 #   ##mutate(perc_UDA_deliverd = UDA_delivery * 100 / contracted_UDAs) %>%
 #   add_row(month = seq(as.Date("2016-04-01"), as.Date("2023-03-01"), lubridate::month(1))) %>%
@@ -94,12 +94,12 @@
 #                                     month >= as.Date("2023-04-01") & month < as.Date("2024-04-01") ~ "2023/24",
 #                                     month >= as.Date("2024-04-01") & month < as.Date("2025-04-01") ~ "2024/25")) %>%
 #   left_join(historic_data, by = "financial_year") %>%
-#   mutate(plot_UDA_delivery = if_else(!is.na(UDA_delivery), UDA_delivery , full_year_UDA_delivery)) 
+#   mutate(plot_UDA_delivery = if_else(!is.na(UDA_delivery), UDA_delivery , full_year_UDA_delivery))
 # 
 # 
 # data <- data %>%
-#   mutate(year_type = if_else(financial_year == max(data$financial_year), 
-#                              "Current Financial Year", 
+#   mutate(year_type = if_else(financial_year == max(data$financial_year),
+#                              "Current Financial Year",
 #                              "Previous Financial Years")) %>%
 #   #get one ro wper prev financial year
 #   filter((substr(month, 6, 7) == "04" & substr(month, 9, 10) == "01")| year_type == "Current Financial Year")
@@ -143,11 +143,11 @@
 #   #                    breaks = seq(0, max(c(data$plot_UDA_delivery, 95), na.rm = T) + 5, 10)#,
 #   #                    #labels = scales::percent_format(accuracy = 1)
 #   # ) +
-#   
+# 
 #   labs(title = "Time series of banded Courses of Treatment*",
 #        x = "Time period",
 #        y = "UDAs delivered",
 #        subtitle = paste0(subtitle, ": ", subtitle_addition),
 #        caption = "* Foundation Dentists Excluded") +
 #   theme(axis.text.x = element_text(angle = 90, vjust=-0.0001
-#   )) 
+#   ))
---
+++ R/03 plot_functions/plot_unique_patients_rolling_as_percent_of_February_2020.R
@@ -0,0 +1,164 @@
+################################################################################
+#function to plot unique patients by band
+plot_unique_patients_rolling_as_percent_of_feb_20 <- function(data = unique_patients_rolling,
+                                         scheduled_data = UDA_scheduled_data,
+                                         level = "National",
+                                         region_STP_name = NULL,
+                                         plotChart = TRUE,
+                                         remove_prototypes = FALSE,
+                                         get_perc = FALSE,
+                                         all_regiona_and_STPs = FALSE,
+                                         contract_level = FALSE,
+                                         asIndex = FALSE){
+  
+  #avoid standard form notation
+  options(scipen = 5)
+  
+  data <- data %>%
+    rename(month = month_ending)
+  
+  #join annual contracted UDAs
+  contracted_UDAs <- scheduled_data %>%
+    filter(month == max(scheduled_data$month, na.rm = TRUE)) %>%
+    select(contract_number, annual_contracted_UDA)
+  
+  data <- data %>%
+    left_join(contracted_UDAs, by = "contract_number")
+  
+  #remove prototype contracts if specified
+  if(remove_prototypes){
+    #create not in function
+    `%notin%` = Negate(`%in%`)
+    data <- data %>%
+      filter(!(contract_number %in% prototype_contracts$prototype_contract_number & month < as.Date("2022-04-01"))) %>%
+      filter(annual_contracted_UDA > 100)
+    
+    chartCaption <- "*N.B. this analysis uses unique patients per contract** and does not take \ninto account patients who have been seen at more than one dental practice. \n**EXCLUDING contracts with annual contracted UDA < 100. Excluding prototype contracts up until April 2022."
+  }else{
+    
+    chartCaption <- "*N.B. this analysis uses unique patients per contract** and does not take \ninto account patients who have been seen at more than one dental practice. \n**INCLUDING contracts with annual contracted UDA < 100 and prototype contracts."
+  }
+  
+  #filter for region or STP
+  if(level == "Regional"){
+    data <- data %>%
+      filter(region_name == region_STP_name)
+    subtitle <- region_STP_name
+  }else if(level == "STP"){
+    data <- data %>%
+      filter(commissioner_name == region_STP_name)
+    subtitle <- region_STP_name
+  }else{
+    subtitle <- "England"
+  }
+  
+  #group by different levels depending on inputs
+  if(all_regiona_and_STPs == TRUE){
+    data <- data %>%
+      group_by(month, region_name, commissioner_name)
+    
+  }else if(contract_level == TRUE){
+    
+    data <- data %>%
+      group_by(month, contract_number, region_name, commissioner_name)
+    
+  }else{
+    data <- data %>%
+      group_by(month)
+  }
+  
+  data_to_print <- data %>%
+    summarise(unique_patients_rolling_12M = sum(unique_patients_rolling_12M, na.rm = TRUE),
+              band1_unique_patients_rolling_12M = sum(band1_unique_patients_rolling_12M, na.rm = TRUE),
+              band2_or_3_unique_patients_rolling_12M = sum(band2_or_3_unique_patients_rolling_12M, na.rm = TRUE),
+              band1_urgent_unique_patients_rolling_12M = sum(band1_urgent_unique_patients_rolling_12M, na.rm = TRUE),
+              band_other_unique_patients_rolling_12M = sum(band_other_unique_patients_rolling_12M, na.rm = TRUE)) 
+  
+  data <- data_to_print %>%
+    pivot_longer(c(unique_patients_rolling_12M,
+                   band1_unique_patients_rolling_12M,
+                   band2_or_3_unique_patients_rolling_12M,
+                   band1_urgent_unique_patients_rolling_12M,
+                   band_other_unique_patients_rolling_12M)) %>%
+    rename(band = name, total_unique_patients = value) %>%
+    mutate(band = case_when(band == "unique_patients_rolling_12M" ~ "Any band",
+                            band == "band1_unique_patients_rolling_12M" ~ "Band 1",
+                            band == "band2_or_3_unique_patients_rolling_12M" ~ "Band 2 or 3",
+                            band == "band1_urgent_unique_patients_rolling_12M" ~ "Urgent band 1",
+                            band == "band_other_unique_patients_rolling_12M" ~ "Other band")) %>%
+    mutate(month = as.Date(month))
+  
+  data$band <- factor(data$band,
+                      levels = c(
+                        "Any band",
+                        "Band 1",
+                        "Band 2 or 3",
+                        "Urgent band 1",
+                        "Other band"
+                      ))
+  
+  #set everything as indext of April 2019
+  if(asIndex == TRUE){
+    
+    feb2020 <- data %>%
+      filter(month == as.Date("2020-02-01")) %>%
+      select(-month,
+             feb2020_total_unique_patients = total_unique_patients)
+    
+    data <- data %>%
+      left_join(feb2020, by = "band") %>%
+      mutate(total_unique_patients = total_unique_patients * 100 / feb2020_total_unique_patients)
+    
+    title <- "Total number of unique patients seen over a rolling 12 month period by band \nas a percentage of February 2020 figures"
+    ylab <- "Percentage of February 2020 unique patients"
+    
+  }else{
+    title <- "Total number of unique patients seen over a rolling 12 month period by band"
+    ylab <- "Unique patients"
+  }
+  
+  
+  if(plotChart == TRUE & get_perc == FALSE){
+    #plot code
+    ggplot(data) +
+      theme_bw() +
+      geom_line(aes(x = month,
+                    y = total_unique_patients,
+                    colour = band)) +
+      geom_point(aes(x = month,
+                     y = total_unique_patients,
+                     colour = band)) +
+      scale_y_continuous(labels=function(x) format(x, big.mark = ",", scientific = FALSE)) +
+      scale_colour_manual(values = get_colour_palette()) +
+      labs(title = title,
+           x = "12 month rolling period end date",
+           y = ylab,
+           subtitle = subtitle,
+           caption = chartCaption
+      ) +
+      scale_x_date(date_breaks = "1 month", 
+                   date_labels = "%b-%y") +
+      theme(axis.text.x = element_text(angle = 90))
+    #theme(legend.position = "bottom")
+  }else if(plotChart == FALSE & get_perc == FALSE){
+    data_to_print <- data_to_print %>%
+      mutate(month = as.Date(month)) %>%
+      rename(`12 month period end` = month,
+             `Region Name` = region_name,
+             `Commissioner Name` = commissioner_name,
+             `Any band unique patients rolling 12 month` = unique_patients_rolling_12M,
+             `Band 1 unique patients rolling 12 month` = band1_unique_patients_rolling_12M,
+             `Band 2 or 3 unique patients rolling 12 month` = band2_or_3_unique_patients_rolling_12M,
+             `Urgent band 1 unique patients rolling 12 month` = band1_urgent_unique_patients_rolling_12M,
+             `Other unique patients rolling 12 month` = band_other_unique_patients_rolling_12M
+      )
+  }else{
+    #percentage of pre-covid levels
+    data <- data %>%
+      filter(band == "Any band") %>%
+      arrange(month)
+    
+    round(data$total_unique_patients[nrow(data)] * 100 / data$total_unique_patients[1])
+  }
+  
+}
